04598f4077a0d77bcdc231256a3139d7d69d8406
kyrakura <caidavid13@gmail.com>
2023-10-11 11:56:39 +0300

    lab 4.5 commit2

--- lab 4.5/lab 4.5.cpp
+++ lab 4.5/lab 4.5.cpp
@@ -1,4 +1,4 @@
-// Lab_04_4.cpp
+// Lab_04_5.cpp
 // < Цай Девід >
 // Лабораторна робота № 4.5
 // «Попадання» у плоску фігуру 
@@ -46,32 +46,30 @@ int main() {
     for (int i = 0; i < 10; i++) {
         double r, x, y;
 
-        cout << " r = ";
-        cin >> r;
-        r = abs(r);
-
+        r = abs((4 * rand() / RAND_MAX) - 2); // Генерація випадкового r
         x = (4 * r * rand() / RAND_MAX) - (2 * r); // Генерація випадкового x
         y = (4 * r * rand() / RAND_MAX) - (2 * r); // Генерація випадкового y
 
-        cout << "x = " << x << ", y = " << y << " - ";
+        cout << "r = " << setprecision(4) << fixed << r;
+        cout << ", x = " << setprecision(4) << fixed << x;
+        cout << ", y = " << setprecision(4) << fixed << y;
 
         if ((x >= -2 * r) && (x <= 2 * r) && (y >= -2 * r) && (y <= 2 * r)) {
             if ((x >= 0) && (abs(x) <= r) && (y >= 0) && (abs(y) <= r)) {
-                cout << "yes" << endl;
+                cout << " - yes" << endl;
             }
             else if ((x <= 0) && (abs(x) <= r) && (y <= 0) && (abs(y) <= r)) {
-                cout << "yes" << endl;
+                cout << " - yes" << endl;
             }
             else if ((x <= 0) && (abs(x) <= r) && (y >= 0) && (abs(y) <= r) && (y <= -1 * sqrt(pow(r, 2) - pow(x + r, 2)) + r)) {
-                cout << "yes" << endl;
+                cout << " - yes" << endl;
             }
             else if ((x >= 0) && (abs(x) <= r) && (y <= 0) && (abs(y) <= r) && (y >= sqrt(pow(r, 2) - pow(x - r, 2)) - r)) {
-                cout << "yes" << endl;
+                cout << " - yes" << endl;
             }
             else
-                cout << "no" << endl;
+                cout << " - no" << endl;
         }
     }
-
     return 0;
 }
